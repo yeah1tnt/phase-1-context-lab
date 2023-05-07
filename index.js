baa7881aee53065b128b224e421d336cda7eb010
Tong Tang <pichip22@hotmail.com>
2023-05-07 12:02:21 -0400

    completed

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
 /* Your Code Here */
+function createEmployeeRecord(employee){
+    const employeeRecord = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return employeeRecord;
+}
 
+function createEmployeeRecords(employee){
+    return employee.map(function (record) { 
+        return createEmployeeRecord(record)
+    });
+}
+
+function createTimeInEvent(dateStamp){
+    const [date, hour] = dateStamp.split(` `);
+    const inEvent = {
+        type: "TimeIn",
+        hour: parseFloat(hour),
+        date: date,
+    }
+    this.timeInEvents.push(inEvent);
+    return this;
+}
+
+function createTimeOutEvent(dateStamp){
+    const [date, hour] = dateStamp.split(` `);
+    const outEvent = {
+        type: "TimeOut",
+        hour: parseFloat(hour),
+        date: date,
+    }
+    this.timeOutEvents.push(outEvent);
+    return this;
+}
+
+function hoursWorkedOnDate(date){
+    const inEvent = this.timeInEvents.find(function (inEvent){
+         return inEvent.date === date;
+        });
+    const outEvent = this.timeOutEvents.find(function (outEvent){
+        return outEvent.date === date;
+    });
+    return (outEvent.hour - inEvent.hour)/100;
+}
+
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this,date)*this.payPerHour;
+}
+
+function findEmployeeByFirstName(employeeRecord, name){
+    return employeeRecord.find(function (e) {
+        return e.firstName === name;
+    })
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +79,10 @@ const allWagesFor = function () {
     return payable
 }
 
+function calculatePayroll(arr){
+    let total = 0;
+    arr.forEach(function (employeeRecord){
+        return total = total + allWagesFor.call(employeeRecord);
+    })
+    return total;
+}
